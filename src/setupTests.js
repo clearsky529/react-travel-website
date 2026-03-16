7774a9789ca8fee4e9c1c7d162555106e3de4a12
clearsky529 <clearsky5290@gmail.com>
2026-03-16 16:14:05 -0400

    update for Mar, 16

--- src/setupTests.js
+++ src/setupTests.js
@@ -9,4 +9,4 @@ import '@testing-library/jest-dom';
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
